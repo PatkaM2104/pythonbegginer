e41dced39b358a1af27130453ba33aeb829143f6
PatkaM2104 <patricia.musakova@icloud.com>
2026-01-19 17:27:52 +0100

    Update hodina2.py

--- hodina2.py
+++ hodina2.py
@@ -2,3 +2,24 @@ A = int(input("aké je A: "))
 B = int(input("aké je B: "))
 Súčet = A + B
 print("Súčet je:", Súčet)
+
+
+cislo = int(input("Zadaj číslo: "))
+if cislo % 2 == 0:
+    print("Číslo je párne")
+else:
+    print("Číslo je nepárne")
+
+
+a = int(input("Zadaj šírku (a): "))
+b = int(input("Zadaj výšku (b): "))
+
+for i in range(b):
+    print("*" * a)
+
+
+a = int(input("Zadaj šírku (a): "))
+b = int(input("Zadaj výšku (b): "))
+
+for i in range(b):
+    print("*" * i)
