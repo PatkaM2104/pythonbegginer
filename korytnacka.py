75eb737ac1b95d7e90eeed169df3450533cae208
PatkaM2104 <patricia.musakova@icloud.com>
2026-02-09 18:07:30 +0100

    Create korytnacka.py

---
+++ korytnacka.py
@@ -0,0 +1,26 @@
+from turtle import *
+import turtle
+
+
+korytnacka = turtle.Turtle()
+korytnacka.shape("turtle") # Zmení tvar ukazovateľa na korytnačku
+korytnacka.color("blue")
+
+korytnacka.width(5)
+korytnacka.circle(50)
+korytnacka.right(90)
+korytnacka.forward(20)
+korytnacka.right(90)
+korytnacka.forward(50)
+korytnacka.back(100)
+korytnacka.forward(50)
+korytnacka.left(90)
+korytnacka.forward(80)
+korytnacka.left(45)
+korytnacka.forward(60)
+korytnacka.backward(60)
+korytnacka.right(90)
+korytnacka.forward(60)
+
+
+done()
